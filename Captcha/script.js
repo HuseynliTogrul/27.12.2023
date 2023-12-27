2c3df7efd19c40a9384a65f4da3dd58a9591d7ac
HuseynliTogrul <toghrulih@code.edu.az>
2023-12-28 00:22:42 +0400

    Captcha task end

--- Captcha/script.js
+++ Captcha/script.js
@@ -12,3 +12,45 @@ const input = document.getElementById("input");
 
 let chars = [];
 let captcha = "";
+
+const getRandomChars = () => {
+    const mixedChars = numbers + upperCaseLetters + lowerCaseLetters;
+    return mixedChars[Math.floor(Math.random() * mixedChars.length)];
+}
+
+const generateRandomChars = () => {
+    for (let i = 1; i < 7; i++) {
+        captcha += getRandomChars();
+    }
+
+    captchaText.textContent = captcha;
+    
+}
+
+generateRandomChars()
+
+input.addEventListener("input",(e) => {
+    const inputValue = e.target.value;
+    
+    if(inputValue === captcha){
+        input.classList.add("ring-green-600")
+        btn.classList.add("bg-green-500", "cursor-pointer")
+    }else{
+        input.classList.remove("ring-green-600")
+        btn.classList.remove("bg-green-500", "cursor-pointer")
+    }
+
+    if(inputValue !== captcha && inputValue.length === 6){
+        input.classList.add("ring-red-600")
+    }else{
+        input.classList.remove("ring-red-600")
+    }
+});
+
+input.addEventListener("keypress",(e) => {
+    const inputValue = e.target.value;
+
+    if(inputValue.length === 6){
+        e.preventDefault();
+    }
+});
